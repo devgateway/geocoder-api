7afb8a38f8b2196d551dad0eacbbbd3bb566c6da
Sebastian Dimunzio <sdimunzio@gmail.com>
2017-12-09 00:39:56 -0300

    Deployment configuration - added static ui path configuration

---
+++ src/main/java/org/devgateway/geocoder/MvcConfig.java
@@ -0,0 +1,28 @@
+package org.devgateway.geocoder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${geocoder.ui.path}")
+    private String uiPath;
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("redirect:index.html");
+
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (uiPath != null) {
+            registry.addResourceHandler("/**").addResourceLocations("file:" + uiPath);
+        }
+    }
+}

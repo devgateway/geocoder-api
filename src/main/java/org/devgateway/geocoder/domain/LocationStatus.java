60027ea48933b2d2ebadcea5e0d9070cfda4b9b2
Ionut Dobre <idobre@developmentgateway.org>
2017-12-14 13:01:35 +0200

    change the name for autogeocoded location status.

--- src/main/java/org/devgateway/geocoder/domain/LocationStatus.java
+++ src/main/java/org/devgateway/geocoder/domain/LocationStatus.java
@@ -4,6 +4,13 @@ package org.devgateway.geocoder.domain;
  * Created by Sebastian Dimunzio on 11/15/2017.
  */
 public enum LocationStatus {
+    AUTO_CODED,                 // this location was added by the auto geocoder tool
 
-    NEW_AUTO_CODE, EXISTING, NEW, UPDATED, DELETED
+    EXISTING,                   // this location was already in the IATI activity
+
+    NEW,                        // this is a location that was added by the user
+
+    UPDATED,                    // this is a location that was updated by the user
+
+    DELETED                     // thi is a location that was deleted by the user
 }

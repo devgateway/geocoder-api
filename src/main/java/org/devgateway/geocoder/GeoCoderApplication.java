c70cf91e55fb87ccb8feba6c33a0261da1255396
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-01-08 11:21:23 -0300

    adding PropertySource

--- src/main/java/org/devgateway/geocoder/GeoCoderApplication.java
+++ src/main/java/org/devgateway/geocoder/GeoCoderApplication.java
@@ -20,11 +20,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 
 @SpringBootApplication
 @EnableCaching
+@PropertySource("classpath:/application.properties")
 public class GeoCoderApplication {
 
     public static void main(String[] args) throws Exception {

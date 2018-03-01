d57da63703b1a9011fa4ae9cfd4262dda3c758c6
Sebastian Dimunzio <sdimunzio@gmail.com>
2018-02-28 23:05:49 -0300

    removing env variables failing on staging

--- src/main/resources/sql/initial_data.sql
+++ src/main/resources/sql/initial_data.sql
@@ -6,15 +6,7 @@
 -- Dumped by pg_dump version 9.6.6
 
 -- Started on 2018-02-08 15:27:12 -03
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- TOC entry 1 (class 3079 OID 12429)
